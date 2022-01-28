712d41706a9843130e4d1dc4d3a2adaba6fa7b7c
KG1610 <kylejardine@me.com>
2022-01-28 15:11:06 +0200

    clean up

--- Test.py
+++
@@ -1,6 +0,0 @@
-message = 'Home machine Synced Folder'
-wm = "WOrk Mahchine Synced"
-hm = 'Ok nice happy with this. Python efforts across home and work notebooks synced'
-print(message)
-print(wm)
-print(hm)
